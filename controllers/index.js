b74dc7ea4d95cda6f69dbbd94c59a38f27c937df
Jordan1211 <gre20057@byui.edu>
2022-11-02 13:54:21 -0600

    fix user route

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const index = (req, res) => {
   res.send(
-    'This is the starting point for the custody API here are the endpoints for use: <br> /api-docs <br> /holiday <br> /family <br> /finance <br> /user admin <br> /authorization - in order to make calls as a logged in usery'
+    'This is the starting point for the custody API here are the endpoints for use: <br> /api-docs <br> /holiday <br> /family <br> /finance <br> /user_admin <br> /authorization/login - in order to make calls to /user endpoint as a logged in user'
   );
 };
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ router.get('/', index);
 router.use('/', require('./swagger'));
 router.use('/authorization', require('./authorization'));
 router.use('/user_admin', require('./user_admin'));
+router.use('/user', require('./user'));
 router.use('/holiday', require('./holiday'));
 router.use('/family', require('./family'));
 router.use('/finance', require('./finance'));

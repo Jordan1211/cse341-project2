46f11e57a3ec15d8401da9a210e86b96a876c4b1
Jordan1211 <gre20057@byui.edu>
2022-11-02 21:21:22 -0600

    try again for min requirements

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const { index } = require('../controllers');
 const router = require('express').Router();
 
-const loadUser = require('../middleware/loadUser');
-
 router.get('/', index);
 router.use('/', require('./swagger'));
 router.use('/authorization', require('./authorization'));
 router.use('/user_admin', require('./user_admin'));
-router.use('/user', loadUser, require('./user'));
+router.use('/user', require('./user'));
 router.use('/holiday', require('./holiday'));
-router.use('/family', loadUser, require('./family'));
-router.use('/finance', loadUser, require('./finance'));
+router.use('/family', require('./family'));
+router.use('/finance', require('./finance'));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const UsersController = require('../controllers/user');
+const loadUser = require('../middleware/loadUser');
 
 const router = Router();
+
+router.use([loadUser]);
 
 router.get('/', UsersController.UsersController.index);
 
